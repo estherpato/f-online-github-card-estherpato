ca425c772b491584a3c179e34f03fc3c45fe0013
estherpato <estherpato@gmail.com>
2018-11-13 15:31:02 +0100

    WIP fetching all data

--- src/components/App.js
+++ src/components/App.js
@@ -12,18 +12,60 @@ class App extends Component {
   }
 
   componentDidMount() {
-    if (this.state.adalabUsers.length === 0) {
+    // if (this.state.adalabUsers.length === 0) {
       this.getUsers();
-    }
+    // }
   }
 
   getUsers() {
     fetch('https://api.github.com/orgs/adalab/members?per_page=75')
       .then(res => res.json())
       .then(users => {
-        this.setState({ adalabUsers: users })
-        this.setLocalStorage(users)
+        console.log(users)
+        for(let i = 0; i < users.length ; i++)
+        {  
+        this.getUserData(users[i].url)
+      }
+        // this.setLocalStorage(users)
       })
+  }
+
+  getUserData(url) {
+    fetch(url)
+    .then(res => console.log(res))
+    // .then(data => {
+    //   this.getUserRepos(data.repos_url)
+    //   this.getUserFollowers(data.followers_url)
+    //   this.getUserFollowing(data.following.url)
+    // })
+  }
+
+  getUserRepos(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log('user repo', data)
+    })
+  }
+
+  getUserFollowers(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log('user followers', data)
+    })
+  }
+
+  getUserFollowing(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log('user following', data)
+    })
+  }
+
+  setUserArray() {
+
   }
 
   setLocalStorage(users) {
