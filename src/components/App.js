14ff549466de2cf015e5d9d9d996ee2677f9741b
estherpato <estherpato@gmail.com>
2018-11-13 14:18:45 +0100

    Add fetch call to github api and store data in local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
-//const url = https://api.github.com/orgs/adalab/members?per_page=75;
+// const url = https://api.github.com/orgs/adalab/members?per_page=75;
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      adalabUsers: JSON.parse(localStorage.getItem("users")) || [],
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.adalabUsers.length === 0) {
+      this.getUsers();
+    }
+  }
+
+  getUsers() {
+    fetch('https://api.github.com/orgs/adalab/members?per_page=75')
+      .then(res => res.json())
+      .then(users => {
+        this.setState({ adalabUsers: users })
+        this.setLocalStorage(users)
+      })
+  }
+
+  setLocalStorage(users) {
+    localStorage.setItem("users", JSON.stringify(users));
   }
   
   render() {
     return (
       <div className="App">
-        
+
       </div>
     );
   }
