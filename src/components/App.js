04ae18bc17f292e6d68e7c409482905ff6adcee7
estherpato <estherpato@gmail.com>
2018-11-13 13:59:04 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+//const url = https://api.github.com/orgs/adalab/members?per_page=75;
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
